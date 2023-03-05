71f2d595d87c7c20c767713e2e13b220cb0fb018
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-03-05 04:37:23 +0000

    starting react, /* allows nested routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt') //hash the password, don't save a plain text pa
 // @access Private
 const getAllUsers = asyncHandler(async (req,res) =>{
     const users = await User.find().select('-password').lean() //stops from getting a long document
-    if (!users){
+    if (!users?.length){ //does this have any length, says !false because there's no length
         return res.status(400).json({message:"No users found"})
     }
     res.json(users)
@@ -43,7 +43,7 @@ const createNewUser = asyncHandler(async (req,res) =>{
 const updateUser = asyncHandler(async (req,res) =>{
     const {id,username, roles, active, password} = req.body
     //confirm data
-    if(!id || !username ||!Array.isArray(roles) || !roles.length || typeof active !== 'boolean'){
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean'){
         return res.status(400).json({message:"All fields are required"})
     }
     const user = await User.findById(id).exec()
@@ -53,18 +53,42 @@ const updateUser = asyncHandler(async (req,res) =>{
     //check for duplicate
     const duplicate = await User.findOne({username}).lean().exec
     //Allow updates to the original user
-    if(duplicate && duplicate?._id.toString() !== id){
+    if(duplicate && duplicate?.id.toString() !== id){ //problem
         return res.status(409).json({message:'Duplicate username'})
     }
     user.username = username
     user.roles = roles
     user.active = active
+
+    if(password){
+        //hashing password
+        user.password = await bcrypt.hash(password,10)
+    }
+    const updatedUser = await user.save()
+    res.json({message: `${updatedUser.username} updated`})
+
 })
 // @desc Delete the user
 // @route DELETE /users
 // @access Private
 const deleteUser = asyncHandler(async (req,res) =>{
-    
+    const {id} = req.body
+    if (!id) {
+        return res.status(400).json({message:'User ID required'})
+    }
+    const note = await Note.findOne({user: id}).lean().exec()
+    if (note?.legnth){
+        return res.status(400).json({message:"User has assigned notes"})
+    }
+    const user = await User.findById(id).exec()
+
+    if(!user){
+        return res.status(400).json({message:"User not found"})
+    }
+    const result = await user.deleteOne(); //user deleted but the result will hold the deleted user's info
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`;
+    res.json(reply);
 })
 
 module.exports = {
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const AutoIncremenet = require('mongoose-sequence')(mongoose)
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 const noteSchema = new mongoose.Schema(
         {
         user:{
